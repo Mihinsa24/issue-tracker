4b8c87b0c39d4053f624e04158bb7e2b65c9d23d
Mihinsa Nakandala <mihinsanakandala@Mihinsas-MacBook-Air.local>
2026-05-23 21:44:54 +0530

    updated the frontend hosted url in the index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-netlify-app.netlify.app"
+    "https://tracer-app.netlify.app/"
   ],
   credentials: true,
 }));
