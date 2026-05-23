5c1735edc7713d700e6463ba2cd7f59a09359e3b
Mihinsa Nakandala <mihinsanakandala@Mihinsas-MacBook-Air.local>
2026-05-23 21:32:02 +0530

    updated the backend hosted url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://issue-tracker-api-4kd5.onrender.com/api",
 });
 
 // Automatically attach token to every request
